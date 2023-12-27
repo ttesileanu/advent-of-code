1d1ddb527a0dcd7023bd331d7dd62be9b15d0fa3
Tiberiu Tesileanu <ttesileanu@gmail.com>
2023-12-27 10:33:50 -0500

    Factor out common day-11 code

---
+++ 2023/common11.py
@@ -0,0 +1,52 @@
+from types import SimpleNamespace
+from typing import List, Tuple
+
+from utils import Matrix
+
+
+def parse_image(img: Matrix) -> SimpleNamespace:
+    galaxies = []
+    empty_rows = []
+    for i, row in enumerate(img.data):
+        empty_row = True
+        for j, elem in enumerate(row):
+            if elem == "#":
+                galaxies.append((i, j))
+                empty_row = False
+            else:
+                assert elem == "."
+
+        if empty_row:
+            empty_rows.append(i)
+
+    empty_cols = []
+    for j in range(img.ncols):
+        empty_col = True
+        for i in range(img.nrows):
+            if img[i, j] != ".":
+                empty_col = False
+
+        if empty_col:
+            empty_cols.append(j)
+
+    res = SimpleNamespace(
+        galaxies=galaxies, empty_rows=empty_rows, empty_cols=empty_cols
+    )
+    return res
+
+
+def _expanded_1d(i1: int, i2: int, empty: List[int]) -> int:
+    m = min(i1, i2)
+    M = max(i1, i2)
+
+    to_expand = [_ for _ in empty if m <= _ <= M]
+    return abs(M - m) + len(to_expand)
+
+
+def expanded_distance(
+    a: Tuple[int, int], b: Tuple[int, int], empty_rows: List[int], empty_cols: List[int]
+) -> int:
+    """This is Manhattan distance, with some columns or rows expanded."""
+    d_rows = _expanded_1d(a[0], b[0], empty_rows)
+    d_cols = _expanded_1d(a[1], b[1], empty_cols)
+    return d_rows + d_cols

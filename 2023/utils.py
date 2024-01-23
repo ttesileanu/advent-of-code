5077e7881d4bae5f95e83676f23671cd87c7cd75
Tiberiu Tesileanu <ttesileanu@gmail.com>
2024-01-23 06:44:29 -0500

    Factor IntegerLattice out of day 8 into utils

--- 2023/utils.py
+++ 2023/utils.py
@@ -313,6 +313,160 @@ def loadmatrix() -> Matrix[str]:
     return matrix
 
 
+def extended_gcd(a: int, b: int, /) -> Tuple[int, int, int]:
+    """Find GCD and Bézout coefficients.
+
+    From https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode.
+
+    Parameters
+    ----------
+    a, b : int
+        Integers for which to find the GCD.
+
+    Returns
+    -------
+    gcd : int
+        The GCD between `a` and `b`.
+    x, y : int
+        The Bézout coefficients, obeying `a * x + b * y = gcd`.
+    """
+    s = 0
+    r = b
+
+    old_s = 1
+    old_r = a
+
+    while r != 0:
+        q = old_r // r
+
+        old_s, s = s, old_s - q * s
+        old_r, r = r, old_r - q * r
+
+    gcd = old_r
+    x = old_s
+    if b != 0:
+        y = (old_r - old_s * a) // b
+    else:
+        y = 0
+
+    return gcd, x, y
+
+
+@dataclasses.dataclass
+class IntegerLattice:
+    """An integer lattice, `base + k * step` for integer `k`.
+
+    `base = None` stands for empty.
+    """
+
+    base: Union[int, None]
+    step: Union[int, None]
+
+    def intersect(self, other: "IntegerLattice") -> "IntegerLattice":
+        """Return a new integer lattice that is the intersection of the two."""
+        # The intersection is the lattice of integers n where
+        #       n = base1 + step1 * k1 ,                                (1)
+        #       n = base2 + step2 * k2 ,
+        # for some integers k1 and k2.
+        #
+        # This implies
+        #       base2 - base1 = step1 * k1 - step2 * k2 .              (2)
+        # Bézout's identity implies that a solution exists iff
+        #       base2 - base1 = delta = m * gcd(step1, step2) ,
+        # where m is some integer.
+        #
+        # In that case, let x, y be Bézout coefficients, i.e.,
+        #       gcd = step1 * x + step2 * y .
+        # Then
+        #       step1 * (x * m) - step2 * (-y * m) = m * gcd = base2 - base1 ,
+        # which implies that a solution for equation (2) above is
+        #       k1 = x * m ,  k2 = -y * m
+        #
+        # All pairs of solutions are given by
+        #       (k1 + p * step2 / gcd, k2 + p * step1 / gcd)
+        # where p is an integer.
+        #
+        # Plugging this into equation (1) above, we get that the integers n on the
+        # intersection lattice obey
+        #       n = base1 + x * m * step1 + p * step1 * step2 / gcd
+        #         = base1 + x * m * step1 + p * lcm ,
+        # where lcm is the least common multiplier, lcm = step1 * step2 / gcd.
+        #
+        # Similarly,
+        #       n = base2 - y * m * step2 + p * step1 * step2 / gcd
+        #         = base2 - y * m * step2 + p * lcm
+        #         = base1 + (base2 - base1) - y * m * step2 + p * lcm
+        #         = base1 + m * (gcd - y * step2) + p * lcm
+        #         = base1 + m * x * step1 + p * lcm ,
+        # where we used Bézout's identity. This matches the above.
+
+        # In other words, the new lattice is given by
+        #       base = base1 + x * m * step1
+        #            = base1 + x * (base2 - base1) / gcd * step1
+        #       step = lcm = step1 * step2 / gcd .
+
+        if self.empty() or other.empty():
+            return IntegerLattice(None, None)
+
+        if self.step == 0 and other.step == 0:
+            if self.base == other.base:
+                return IntegerLattice(self.base, 0)
+            else:
+                return IntegerLattice(None, None)
+
+        gcd, x, _ = extended_gcd(self.step, other.step)
+
+        delta = other.base - self.base
+        if delta % gcd != 0:
+            return IntegerLattice(None, None)
+
+        m = delta // gcd
+        new_base = self.base + x * m * self.step
+        new_step = self.step // gcd * other.step
+        return IntegerLattice(new_base, new_step)
+
+    def __and__(self, other: "IntegerLattice") -> "IntegerLattice":
+        return self.intersect(other)
+
+    def empty(self) -> bool:
+        """Return true if this lattice is empty."""
+        return self.base is None
+
+    def first_after(self, start: int) -> Union[int, None]:
+        """Find the smallest entry in the lattice that is larger than or equal to a
+        starting point.
+
+        Returns `None` if the lattice is empty or it contains a single element that is
+        smaller than `start`.
+        """
+        # we have
+        #       base + k * step >= start
+        #   =>  k >= (start - base) / step
+        # smallest integer larger than or equal to something is the ceil:
+        #       k = ceil((start - base) / step) .
+
+        if self.empty():
+            return None
+        if self.step == 0:
+            if self.base >= start:
+                return self.base
+            else:
+                return None
+
+        # can't use floating point because of overflow
+        # instead use:
+        #       ceil(n / m) = floor((n - 1) / m) + 1
+        # (https://en.wikipedia.org/wiki/Floor_and_ceiling_functions)
+        k = (start - self.base - 1) // self.step + 1
+        return self.base + k * self.step
+
+    def __repr__(self) -> str:
+        if self.empty():
+            return "<EMPTY>"
+        else:
+            return f"<{self.base} + k * {self.step}>"
+
+
 def itermatrix() -> Iterator[Matrix[str]]:
     data = []
     for line in iterinput():
